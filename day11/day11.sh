4528de670626b53c0348f96380c3ea37eb56424b
Paul Lampert <apl99@mac.com>
2022-12-12 16:06:50 -0500

    make functions

--- day11/day11.sh
+++ day11/day11.sh
@@ -20,122 +20,152 @@ function Error
 }
 
 
+function parseMonkeys
+{
+    nameref monkeys=$1
 
-# Parse monkey definitions
+    typeset -C newMonkey=(inspectionCount=0)   # initialized monkey.
+    typeset -C m=newMonkey
 
-typeset -g -a monkeys=()           # array of monkeys
-typeset -C m=(inspectionCount=0)   # initialized monkey.
+    while true;do
 
-while true;do
+        read line
+        let rc=$?                 # save for eof loop break
+        if [[ -z $line ]]; then   # empty line denotes end of monkey definition
+            eval monkeys+=$m      # add last monkey to array, if any data in there
 
-    read line
-    let rc=$?                 # save for eof loop break
-    if [[ -z $line ]]; then   # empty line denotes end of monkey definition
-        eval monkeys+=$m      # add last monkey to array, if any data in there
+            m=newMonkey           # initialize monkey.
 
-        typeset -C m=(inspectionCount=0)   # initialize monkey.
+            (( $rc )) && break    # if rcode from read, this means EOF, so break.
+            continue
+        fi
 
-        (( $rc )) && break    # if rcode from read, this means EOF, so break.
-        continue
-    fi
+        set - $line    # split words in to $1,$2,... 
+        case $1 in
+            Monkey)    # Monkey #
 
-    set - $line    # split words in to $1,$2,... 
-    case $1 in
-        Monkey)    # Monkey #
+                # verify monkey number matches array.
+                (( ${2%:} != ${#monkeys[*]} )) && Error "mismatched monkey num $2 != ${#monkeys[*]}"
+                ;;
 
-            # verify monkey number matches array.
-            (( ${2%:} != ${#monkeys[*]} )) && Error "mismatched monkey num $2 != ${#monkeys[*]}"
-            ;;
+            Starting)       # Starting items: 1, 2, 3
 
-        Starting)       # Starting items: 1, 2, 3
+                [[ $2 != "items:" ]] && Error "parse $line"  # sanity checks
+                shift 2
+                m.items=( ${*//,/} )   # strip "," out and create array, ie (1 2 3)
+                ;;
 
-            [[ $2 != "items:" ]] && Error "parse $line"  # sanity checks
-            shift 2
-            m.items=( ${*//,/} )   # strip "," out and create array, ie (1 2 3)
-            ;;
+            Operation:)     # Operation: new = old + 3
 
-        Operation:)     # Operation: new = old + 3
-
-            shift
+                shift
             m.operation="$@"
             ;;
 
-        Test:)          # Test: divisible by 17
+            Test:)          # Test: divisible by 17
 
-            [[ $2 != "divisible" || $3 != "by" ]] && Error "parse $line"  # sanity checks
-            m.testDivisibleBy=$4
-            ;;
+                [[ $2 != "divisible" || $3 != "by" ]] && Error "parse $line"  # sanity checks
+                m.testDivisibleBy=$4
+                ;;
 
-        If)             # If true: throw to monkey 0
-                        # If false: throw to monkey 1'
+            If)             # If true: throw to monkey 0
+                            # If false: throw to monkey 1'
 
-            [[ $3 != throw || $4 != to || $5 != monkey ]] && Error "parse $line"
-            if [[ $2 = true: ]]; then
-                m.ifTrueThrowTo=$6
-            elif [[ $2 = false: ]]; then
-                m.ifFalseThrowTo=$6
-            else
-                Error "parse $line"
-            fi
-            ;;
-    esac
-done < $INPUT
+                [[ $3 != throw || $4 != to || $5 != monkey ]] && Error "parse $line"
+                if [[ $2 = true: ]]; then
+                    m.ifTrueThrowTo=$6
+                elif [[ $2 = false: ]]; then
+                    m.ifFalseThrowTo=$6
+                else
+                    Error "parse $line"
+                fi
+                ;;
+        esac
+    done < $INPUT
+
+    if ((verbose)); then
+        echo "-- Monkey defintions"
+        for ((i=0; i<${#monkeys[*]}; i++));do
+            print -n "$i "
+            typeset -p monkeys[$i]
+            echo
+        done
+        echo "-- end Monkey defintions"
+    fi
+}
+
+
+function runSimulation
+{
+    nameref m=$1
+    typeset numRounds=$2
+    typeset worryDiv=$3
 
 if ((verbose)); then
-    echo "-- Monkey defintions"
-    for ((i=0; i<${#monkeys[*]}; i++));do
+    echo "-- simulation Monkey defintions"
+    for ((i=0; i<${#m[*]}; i++));do
         print -n "$i "
-        typeset -p monkeys[$i]
+        typeset -p m[$i]
         echo
     done
     echo "-- end Monkey defintions"
 fi
 
-# run simulation
+    for round in {1..20}; do
 
-for round in {1..20}; do
+        for ((i=0; i<${#m[*]}; i++));do
 
-    for ((i=0; i<${#monkeys[*]}; i++));do
+            # move items to temp variable
+            unset items
+            typeset -m items=m[i].items
 
-        # move items to temp variable
-        typeset -m items=monkeys[i].items
+            (( m[i].inspectionCount += ${#items[*]} ))   # count inspections per monkey
 
-        (( monkeys[i].inspectionCount += ${#items[*]} ))   # count inspections per monkey
+            typeset -f new                     # new set to float because integers overflow
+            for old in ${items[*]};do
+                (( ${m[i].operation} ))  # execute formula new=old*...
+                (( new/=3 ))                   # worry decay
+                if (( new%${m[i].testDivisibleBy}==0)); then
+                    (( throwTo=${m[i].ifTrueThrowTo} ))
+                else
+                    (( throwTo=${m[i].ifFalseThrowTo} ))
+                fi
+                m[$throwTo].items+=($new)  # move item to monkey list
+            done
 
-        typeset -f new                     # new set to float because integers overflow
-        for old in ${items[*]};do
-            (( ${monkeys[i].operation} ))  # execute formula new=old*...
-            (( new/=3 ))                   # worry decay
-            if (( new%${monkeys[i].testDivisibleBy}==0)); then
-                (( throwTo=${monkeys[i].ifTrueThrowTo} ))
-            else
-                (( throwTo=${monkeys[i].ifFalseThrowTo} ))
-            fi
-            monkeys[$throwTo].items+=($new)  # move item to monkey list
         done
-
+        if ((verbose)); then
+            echo "----after round $round"
+            for ((i=0; i<${#m[*]}; i++));do
+                echo "   monkey $i items ${m[i].items[*]}"
+            done
+        fi
     done
-    if ((verbose)); then
-        echo "----after round $round"
-        for ((i=0; i<${#monkeys[*]}; i++));do
-            echo "   monkey $i items ${monkeys[i].items[*]}"
+
+    if ((verbose));then
+        echo "--- counts after all rounds"
+        for ((i=0; i<${#m[*]}; i++));do
+            echo "   monkey $i counts ${m[i].inspectionCount}"
         done
     fi
-done
+}
 
-if ((verbose));then
-    echo "--- counts after all rounds"
-    for ((i=0; i<${#monkeys[*]}; i++));do
-        echo "   monkey $i counts ${monkeys[i].inspectionCount}"
-    done
-fi
+
+typeset -a initialState
+parseMonkeys initialState
+
+
 
 # Answer 1.  find 2 highest counts and multiply.
 
+
+
+eval typeset -a run1=$(printf %B initialState)
+runSimulation run1 20 3  # numRounds 20, worry divider 3
+
 # index counts
 typeset -A indexCounts
-for ((i=0; i<${#monkeys[*]}; i++));do
-    (( c=${monkeys[i].inspectionCount} ))
+for ((i=0; i<${#run1[*]}; i++));do
+    (( c=${run1[i].inspectionCount} ))
     key=${ printf "count=%010d,monkey=%03d" $c $i;}
     indexCounts[$key]=$c
 done
