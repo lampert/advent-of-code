c2b9dd9bf41e01d326cd2f7e4fd070ea8838bbb3
Paul Lampert <apl99@mac.com>
2023-03-10 19:48:34 -0500

    better version

--- day16/day16.sh
+++ day16/day16.sh
@@ -8,87 +8,77 @@ INPUT=input.txt    # problem data
 
 function shortestDistances
 {
-    # Output dists calculated distance to each cave
-    nameref map=$1
-    nameref dists=$2
-    typeset cave=$3
-    typeset currentDistance=$4
-
-    typeset oldDist
-    oldDist=${dists[$cave]:-inf}    # if unset, then set to infinity
-    if (( currentDistance < oldDist )); then
-        # this is a shorter path.
+    nameref map=$1 dists=$2
+    typeset cave=$3 currentDistance=$4
+    typeset oldDist=${dists[$cave]:-inf}    # if unset, then set to infinity
+    if (( currentDistance<oldDist )); then
         dists[$cave]=$currentDistance
-        # now calculate all tunnels from here
         typeset newCave
-        for newCave in ${map.caves[$cave].tunnels[*]}; do
+        for newCave in ${map.tunnels[$cave][*]}; do
             shortestDistances map dists $newCave $((currentDistance+1))
         done
     fi
 }
 
-typeset globalIterations=0
-typeset globalStart=$SECONDS
-
-function tryPaths
+function calculateScores
 {
     nameref map=$1
-    typeset cave=$2
-    typeset timeLeft=$3
-
-    if (( (++globalIterations % 10000) == 0));then
-        echo "Iterations $globalIterations persec $((int(globalIterations/(SECONDS-globalStart))))"
-    fi
-
-    nameref caveP=map.caves[$cave]              # pointer to current cave info.
-
-    typeset myScore=$((caveP.rate * timeLeft))  # calculate score for turning on this valve.
-
-    # mark as used (rate=0), but remember so I can restore on return.
-    if ((caveP.rate>0)); then
-        unset map.valves[$cave]                 # remove from valve list
-    fi
-
-    # look for valves with rate>0 and try them.
-    typeset c maxScore=0
-    for c in ${!map.valves[*]};do
-        (( map.caves[$c].rate==0 )) && continue # skip no valve
-        typeset -i dist=${caveP.dists[$c]}
-        (( dist+1 > timeLeft )) && continue     # skip if out of time
-        tryPaths map $c $((timeLeft-dist-1))    # get the score for this path
-        (( maxScore=fmax($?,maxScore) ))        # $? has returned score. track maximum
+    typeset cave=$2 timeLeft=$3 score=${4:-0} openValves=${5:-0}
+    ((map.scores[$openValves]=fmax(${map.scores[$openValves]:-0},score))) # max score for this set of open valves
+    typeset c
+    for c in ${!map.valveBit[*]};do                 # calculate from all tunnels
+        typeset bit=${map.valveBit[$c]}
+        (( openValves & bit )) && continue          # skip already open valves
+        typeset distance=${map.dists[$cave][$c]}
+        typeset newTimeLeft=$((timeLeft-distance-1))
+        ((newTimeLeft<=0)) && continue              # can't reach in time
+        calculateScores map $c $newTimeLeft $((score+(map.rate[$c]*newTimeLeft))) $((openValves|bit)) 
     done
-
-    # restore to valve list
-    if ((caveP.rate>0));then
-        map.valves[$cave]=1                     # add back to valve list
-    fi
-    return $((myScore + maxScore))              # return my score
 }
 
-# Read in map of tunnels & rates
-# "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE"
+function readMap
+{
+    nameref map=$1
+    # Read in map of tunnels & rates
+    # "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE"
+    typeset -C map=(typeset -A rate tunnels dists valveBit maxScore scores)
+    while read cv v ch cf r ct cl ct ct2 ts;do
+        typeset r=${r#rate=} r=${r%\;}     # extract rate, remove rate= and ;
+        typeset t=( $ts ) t=${t[*]%,}      # make array, clean up ,'s
+        map.rate[$v]=$r
+        map.tunnels[$v]=$t
+        typeset -A map.dists[$v]
+    done < $INPUT
+    typeset c bit=1
+    for c in ${!map.rate[*]};do            # get distances for every cave to every other cave
+        shortestDistances map map.dists[$c] $c 0
+        if ((map.rate[$c]>0));then         # assign a bit to each valve
+            map.valveBit[$c]=$bit
+            ((bit*=2))
+        fi
+    done
+}
 
-typeset -C map=(typeset -A caves; typeset -A valves)
-while read cv v ch cf r ct cl ct ct2 ts;do
-    typeset r=${r#rate=} r=${r%\;}     # extract rate, remove rate= and ;
-    typeset t=( $ts ) t=${t[*]%,}      # make array, clean up ,'s
-    map.caves[$v]=(rate=$r; typeset -A dists; tunnels=$t )
-    (( r>0 )) && map.valves[$v]=1      # cache list of valves
-done < $INPUT
+typeset map
+readMap map
 
-# Calculate all distances for all caves
-
-typeset c
-for c in ${!map.caves[*]};do
-    shortestDistances map map.caves[$c].dists $c 0
-    echo "Cave $c:"
-    typeset -p map.caves[$c].dists
+# find the max score for all combinations of open valves and 30 minutes overall
+calculateScores map AA 30
+typeset m=0 i
+for i in ${map.scores[*]};do
+    ((m=fmax(m,i)))
 done
+echo "Answer 1: maxScore $m"
 
-echo "Trying paths..."
+# find the max score for 2 disjoint open valve sets, with less time overall
+typeset -A map.scores=()
+calculateScores map AA 26   # recalculate for 26 minutes left.
+typeset bmp1 bmp2 m=0
+for bmp1 in ${!map.scores[*]};do
+    for bmp2 in ${!map.scores[*]};do
+        (( bmp1&bmp2 )) && continue # skip overlapping valves
+        (( m=fmax(m, ${map.scores[$bmp1]}+${map.scores[$bmp2]}) ))  # get max of separate scores
+    done
+done
+echo "Answer 2: maxScore $m"
 
-tryPaths map AA 30
-typeset score=$?
-
-echo "Answer 1: maxScore $score"
