7a572870831ba42432932d0c0e97bbc220aadb77
Paul Lampert <plampert@bloomberg.net>
2024-12-27 10:00:46 -0500

    fixed part 2

--- 2024/7/7.go
+++ 2024/7/7.go
@@ -11,8 +11,10 @@ import (
 // tries all combinations of +/* and returns true any match target
 func tryall(target int, nums []int) (bool,bool) {
     var a string
+	var found1,found2 bool
     permutations:=Pow(3,float64(len(nums)-1)) // n^3 -> # of permutations of + and * and || given # of terms
-    Println(target,nums," perm",permutations)
+	// walk through all possible permutations of operations for this target.
+	// operation "2" (which is combine ||) does not apply to part1
     for ops:=range int(permutations) {
         check:=nums[0]
         a=Sprintf("%v",check)
@@ -27,20 +29,24 @@ func tryall(target int, nums []int) (bool,bool) {
                 check=check+nums[j]
                 a+=Sprintf("+%v",nums[j])
             case 2: // combine
-                mult := Pow(10,Log10(float64(nums[j]))+1.0) // how much to move over left digits
-Println("check",check," mult ",int(mult)," nums[j] ",nums[j]," == ",check*int(mult)+nums[j])
+                mult := Pow10(int(Log10(float64(nums[j]))+1.0)) // how much to move over left digits
                 check=check*int(mult)+nums[j]
                 a+=Sprintf("||%v",nums[j])
                 appliesToPart1=false // don't count this for part 1 answer
             }
             o/=3
         }
-        Printf("%10v == %-10v? %5v part1? %5v  %s\n",target,check,(target==check),appliesToPart1,a)
         if (check==target) {
-            return appliesToPart1,true
+			if appliesToPart1 {
+				found1=true
+			}
+			found2=true
+			if found1 && found2 {
+				break // no need to continue, since both are found
+			}
         }
     }
-    return false,false
+	return found1,found2
 }
 
 func main() {
@@ -59,12 +65,15 @@ func main() {
         // inums is integer slice
         target:=inums[0]
         matchpart1,matchpart2:=tryall(target,inums[1:])
-        if matchpart1 {
-            totalpart1+=target
-        }
-        if matchpart2 {
-            totalpart2+=target
-        }
+		if matchpart1 || matchpart2 {
+			Println("match ",matchpart1,matchpart2," target",target," check ",inums)
+			if matchpart1 {
+				totalpart1+=target
+			}
+			if matchpart2 {
+				totalpart2+=target
+			}
+		}
 	}
     Println("part 1 answer ",totalpart1)
     Println("part 2 answer ",totalpart2)
